e4b2eb95d20b531886735993c37ba454fb7c0c69
hikmatul ulya <72594758+hikmah-ulya@users.noreply.github.com>
2021-12-21 10:10:24 +0700

    Add files via upload

---
+++ TCPSimulation_Client.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+
+namespace TcpSim_Client
+{
+    class TCPSim_client
+    {
+        static IPAddress localAddress = IPAddress.Parse(GetIpAddress());
+
+        public void Start()
+        {
+            try
+            {
+                Console.WriteLine("Client Start");
+                TcpClient clientSocket = new TcpClient();
+                clientSocket.Connect(localAddress, 5000);
+                Console.WriteLine("Server Connected");
+
+                StreamWriter writer = new StreamWriter(clientSocket.GetStream());
+
+                while (true)
+                {
+                    if (clientSocket.Connected)
+                    {
+                        //send message  
+                        string input = Console.ReadLine();
+                        writer.WriteLine(input);
+                        writer.Flush();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+            Console.WriteLine("Press any key to exit from client program");
+
+            Console.ReadKey();
+        }
+
+        public static string GetIpAddress()
+        {
+            IPHostEntry localhost;
+            string localAddress = "";
+
+            // Get the hostname of the local machine
+            localhost = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress address in localhost.AddressList)
+            {
+                // Look for the IPv4 address of the local machine
+                if (address.AddressFamily.ToString() == "InterNetwork")
+                {
+                    // Convert the IP address to a string and return it
+                    localAddress = address.ToString();
+                }
+            }
+            return localAddress;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var client = new TCPSim_client();
+
+            client.Start();
+
+        }
+    }
+}
---
+++ TCPSimulation_Server.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Linq;
+
+
+namespace TcpSim_Server
+{
+    class TCPSim_server
+    {
+        static TcpListener Listener;
+        static TcpClient serverSocket;
+
+        static Int32 port = 5000;
+        static IPAddress localAddress = IPAddress.Parse(GetIpAddress());
+
+        public void Start()
+        {
+            Listener = new TcpListener(localAddress, port);
+            Listener.Start();
+            Console.WriteLine("Server Create " + Listener.LocalEndpoint);
+
+            try
+            {
+                serverSocket = Listener.AcceptTcpClient();
+                Console.WriteLine("Client Accepted");
+
+                while (true)
+                {
+                    ClientListener(serverSocket);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void ClientListener(object argument)
+        {
+            TcpClient tcpClient = (TcpClient)argument;
+            StreamReader reader = new StreamReader(tcpClient.GetStream());
+            StreamWriter writer = new StreamWriter(tcpClient.GetStream());
+
+            Console.WriteLine("Client Connected");
+
+            try
+            {
+                while (true)
+                {
+                    string message = reader.ReadLine();
+                    string chat = "client : " + message;
+                    Console.WriteLine(chat);
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine(" ");
+            }
+            finally
+            {
+                Console.WriteLine("Client Disconnected");
+
+                serverSocket.Client.Disconnect(true);
+                serverSocket.Client.Close();
+                Console.WriteLine("Server Closed");
+                System.Environment.Exit(1);
+
+            }
+
+        }
+
+        public static string GetIpAddress()
+        {
+            IPHostEntry localhost;
+            string localAddress = "";
+
+            // Get the hostname of the local machine
+            localhost = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress address in localhost.AddressList)
+            {
+                // Look for the IPv4 address of the local machine
+                if (address.AddressFamily.ToString() == "InterNetwork")
+                {
+                    // Convert the IP address to a string and return it
+                    localAddress = address.ToString();
+                }
+            }
+            return localAddress;
+        }
+
+    }
+
+    class Program
+    {
+        
+        static public void Main()
+        {
+            var server = new TCPSim_server();
+
+            server.Start();
+        }
+    }
+}
